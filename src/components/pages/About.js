6466994e9a3a88971ec0ebcd2457b097425a0db1
derekjbell <derek.bell10@gmail.com>
2019-03-26 22:52:37 -0700

    Created an about page to learn about routing

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList App that I created to learn React!</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
