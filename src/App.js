e7f6300d4f611d062edd5ea71dbd649d534eea09
derekjbell <derek.bell10@gmail.com>
2019-03-26 22:04:31 -0700

    Created method to add todo items to the list. Added npm uuid for ids

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
 import React, { Component } from 'react';
-import Todos from './components/Todos'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
+
+
 import './App.css';
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: 1,
+    todos: [{
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with friends',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       },
     ]
   }
 
-// Toggles checkbox boolean value
+  // Toggles checkbox boolean value
   checkComplete = (id) => {
-    this.setState ({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo;
-    }) })
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
   }
 
-// Delete Todo item from list
-delTodo = (id) => {
-  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]});
-}
+  // Deletes Todo items from list when you click 'x'
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
+
+// Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
 
   render() {
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} checkComplete={this.checkComplete} delTodo={this.delTodo}/>
+      <div className = "App" >
+        <div className="container">
+        <Header / >
+        <AddTodo addTodo={this.addTodo}/>
+        <Todos todos = {this.state.todos}checkComplete = {this.checkComplete}delTodo = {this.delTodo}/>
+        </div>
       </div>
     );
   }
